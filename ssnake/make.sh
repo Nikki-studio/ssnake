f7cce45ab418803f339b7253ae3f4e1e6be2e7b4
Nikki-studio <joewamwaniki@gmail.com>
2025-11-20 12:31:45 +0300

    Update make.sh

--- ssnake/make.sh
+++ ssnake/make.sh
@@ -1,1 +1,2 @@
 g++ $(find . -name "*.c") $(find . -name "*.cpp") -o ssnake -lncurses -O3 # linux compile
+g++ $(find . -name "*.c") $(find . -name "*.cpp" -o ssnake.exe -lncursesw -O3 # windows compile
